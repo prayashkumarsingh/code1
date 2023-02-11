35d2936aa6899924412ad5b12fbe0f8da300be10
prayashkumarsingh <88892535+prayashkumarsingh@users.noreply.github.com>
2023-02-11 10:52:26 +0530

    Update ServletContextAssignment.java

--- ServletContextAssignment/src/main/java/Assignment/ServletContextAssignment.java
+++ ServletContextAssignment/src/main/java/Assignment/ServletContextAssignment.java
@@ -1,22 +1,12 @@
 package Assignment;
 
-import java.beans.Statement;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Enumeration;
-
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
